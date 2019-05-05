ae72c7ae8bf6d959bd2017e9430e012fdf9c63fa
Syavira Tiara Zulkarnain <39144403+stiaraz@users.noreply.github.com>
2019-05-05 11:44:00 +0700

    Add files via upload

---
+++ src/encrypt/Server.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encrypt;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Owner
+ */
+public class Server extends Thread{
+    ServerSocket server;
+    Socket socket = new Socket();
+    ChatManager cm;
+    ArrayList<PrintWriter> users = new ArrayList();
+    ArrayList<ClientHandler> handlers = new ArrayList();
+    ArrayList <ChatRoomEntry> chatroomEntrys = new ArrayList();
+    ArrayList <ChatCertificate> certificates = new ArrayList();
+    int count;
+    public RSA key;
+    public Server(ChatManager cm)
+    {
+        this.cm = cm;
+        this.key = new RSA();
+        count = 0;
+    }
+    @Override
+    public void run()
+    {
+      try {
+         server = new ServerSocket(0); //server socket listening on port xxxx
+         while(!isInterrupted())
+         {
+             try
+             {
+                 listen();
+             }
+             catch(Exception e)
+             {
+                 
+             }
+         }
+      }
+      catch(Exception e) 
+      {
+         new ErrorFrame("Error with local chatserver");
+      }
+    
+    }
+    public void listen()
+    {
+        try
+        {
+            handlers.add(new ClientHandler(server.accept(), this, count));
+            count++;
+            
+        }
+        catch(Exception e)
+        {
+            
+        }
+    }
+    public ArrayList<PrintWriter> getUsers()
+    {
+        return users;
+    }
+    public ArrayList<ClientHandler> getHandlers()
+    {
+        return handlers;
+    }
+    public int getPort()
+    {
+        return server.getLocalPort();
+    }
+    public void kill()
+    {
+        try
+        {
+            socket.close();
+            server.close();
+            for(ClientHandler h : handlers)
+            {
+                h.kill();
+            }
+            interrupt();
+        }
+        catch(Exception e)
+        {
+                    
+        }
+    }
+}
